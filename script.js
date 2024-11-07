ea09ce7ef38d47cf074fcf51f4f525c316c87a64
Riman Das <34216663+rimanz@users.noreply.github.com>
2024-11-07 15:34:12 +0600

    Add incomplete work in progress intentionally
    Though this commit is incomplete, this will help me to continue working
    from anywhere, specially in this stuck situation.

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-let operand1;
-let operand2;
-let operator;
+// DOM elements
+const display = document.querySelector('.display');
+const keypad = document.querySelector('.keypad');
+const buttons = document.querySelectorAll('button');
 
-function operate() {
+
+// Global variables:
+let operand1 = null;
+let operand2 = null;
+let operator = null;
+let currentNumber = 0;
+
+
+// Event Listeners:
+keypad.addEventListener('click', handleClick);
+
+
+// Operational functions
+function operate(operator) {
+    operand1 = +operand1;
+    operand2 = +operand2;
+    console.log(operand1, operator, operand2);
+
     switch(operator) {
         case '+':
             return add(operand1, operand2);
@@ -22,7 +40,85 @@ function operate() {
         case '√':
             return squareRoot(operand1);
             break;
+        case '√':
+            return squareRoot(operand1);
+            break;
+        case 'C':
+            clear();
+            break;
+        case '⌫':
+            wipeLastDigit();
+            break;
     }
+}
+
+function handleClick(event) {
+    const key = event.target.textContent;
+    const keyType = event.target.className.split(' ')[0];
+
+    if (keyType == 'control') {
+        handleControl(key);
+    } else if (keyType == 'operand') {
+        handleOperand(key);
+    } else if (keyType == 'dot') {
+        handleDot();
+    } else if (keyType == 'operator') {
+        handleOperator(key);
+    }
+}
+
+function handleControl(key) {
+    operate(key);
+}
+
+function handleOperand(operand) {
+    if (currentNumber == '0') {
+        currentNumber = operand;
+    } else {
+        currentNumber += operand;
+    }
+    showNumber(currentNumber);
+}
+
+function handleOperator(key) {
+    if (operand1 == null) {
+        operand1 = currentNumber;
+        currentNumber ='0';
+        operator = key;
+    } else if (operand2 == null) {
+        operand2 == display.textContent;
+        currentNumber = operate(operator);
+        showNumber();
+        operand1 = currentNumber;
+        operand2 = null;
+        currentNumber = 0;
+        operator = key;
+    }
+}
+
+function handleDot() {
+    if (display.textContent.indexOf('.') == -1) {
+        display.textContent += '.';
+        currentNumber = display.textContent;
+    }
+}
+
+function showNumber(num) {
+    display.textContent = num;
+}
+
+function clear() {
+    operand1 = null;
+    operand2 = null;
+    operator = null;
+    currentNumber = 0;
+    display.textContent = currentNumber;
+}
+
+function wipeLastDigit() {
+    const content = display.textContent;
+    display.textContent = content.slice(0, content.length - 1);
+    if (display.textContent == '') display.textContent = 0;
 }
 
 // Arithmetic Functions:
